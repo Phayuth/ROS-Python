0bb79bebedccfb586d26d0411f2dd858d0771f26
Phayuth <70325209+Phayuth@users.noreply.github.com>
2021-02-14 17:17:54 +0700

    Update Code_ROS_Sub_Template.py

--- Code_ROS_Sub_Template.py
+++ Code_ROS_Sub_Template.py
@@ -1,5 +1,5 @@
+#! /usr/bin/env python
 #ROS_Subcriber_Template.py
-#!/usr/bin/env python                      # put this to let the ROS know the environment
 import rospy                               # import rospy lib for ROS
 from geometry_msgs.msg import Twist        # import msg type for ROS  #from [category of the msg] import [ type of the msg , type of the msg]
 
@@ -12,8 +12,8 @@ def twist_callback(msg):                   # create a callback function to feed 
 	rate.sleep()
 
 rospy.init_node('rospy_sub_template')                              # init ros node - rospy.init_node('name of the node')
-sub = rospy.Subscriber('/cmd_vel', Twist, twist_callback)    # init subscriber instance - instance = rospy.Subscriber('/topic_name', msg_type, callback_func)
+sub = rospy.Subscriber('/cmd_vel', Twist, twist_callback)          # init subscriber instance - instance = rospy.Subscriber('/topic_name', msg_type, callback_func)
 rate = rospy.Rate(0.5)                                             # make ros run at specific rate
 
-while not rospy.is_shutdown():                                     # loop 
+while not rospy.is_shutdown():                                     # loop
 	rate.sleep()
